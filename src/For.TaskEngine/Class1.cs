8f216e26401398b6b96f01025a0880a76ea05ec3
Ricky <Ricky@DESKTOP-KEE7AI0>
2018-09-15 21:59:12 +0800

    init

---
+++ src/For.TaskEngine/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace For.TaskEngine
+{
+    public class Class1
+    {
+    }
+}
---
+++ test/For.TaskEngine.ConsoleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace For.TaskEngine.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
